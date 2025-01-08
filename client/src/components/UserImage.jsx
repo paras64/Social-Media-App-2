7c8a7e663a53f386615b7cbdc672ee5a0ddd240e
paras64 <146513526+paras64@users.noreply.github.com>
2025-01-08 18:45:29 +0530

    Second

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://social-media-app-2-blp0.onrender.com/assets/${image}`}
       />
     </Box>
   );
