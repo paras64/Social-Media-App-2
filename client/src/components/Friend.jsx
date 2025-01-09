c1d667630bbc0e5080edc8bfaf09ac78e8340ec7
paras64 <146513526+paras64@users.noreply.github.com>
2025-01-09 16:41:54 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -9,7 +9,7 @@ import UserImage from "./UserImage";
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { _id } = useSelector((state) => state.user);
+  const {_id} = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
 
