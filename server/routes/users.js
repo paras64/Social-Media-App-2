93cee808ae757d44fd4662fb093a59431bb8b78b
paras64 <146513526+paras64@users.noreply.github.com>
2025-01-09 19:01:45 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,7 @@ router.get("/:id" , verifyToken , getUser);
 router.get("users/:id/friends" , verifyToken , getUsersFriends);
 
 // update Routes
-router.patch("/:id/:friendId", verifyToken,addRemoveFriend);
+router.patch("/users/:id/:friendId", verifyToken,addRemoveFriend);
 
 
 export default router;
